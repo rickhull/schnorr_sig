30ec06ff4a065f1d8843d70340fc2c15b64e4ca2
Rick Hull <rick.hull@gmail.com>
2024-05-24 21:27:23 -0400

    match schnorr_sig API

--- lib/schnorr_fast.rb
+++ lib/schnorr_fast.rb
@@ -2,25 +2,37 @@ require 'rbsecp256k1'
 
 module SchnorrFast
   CONTEXT = Secp256k1::Context.create
-  XOPK = Secp256k1::XOnlyPublicKey
-  SSS = Secp256k1::SchnorrSignature
 
-  # returns Secp256k1::SchnorrSignature
-  def self.sign(keypair, msg)
-    raise(TypeError, keypair.inspect) unless keypair.is_a? Secp256k1::KeyPair
-    raise(TypeError, "msg: string") unless msg.is_a? String
-    CONTEXT.sign_schnorr(keypair, msg)
+  def self.sign(sk, m)
+    raise(TypeError, "sk: string") unless sk.is_a? String
+    raise(SizeError, "sk: 32 bytes") unless sk.bytesize == B
+    raise(EncodingError, "sk: binary") unless sk.encoding == Encoding::BINARY
+    raise(TypeError, "m: string") unless m.is_a? String
+
+    CONTEXT.sign_schnorr(key_pair(sk), msg).serialized
   end
 
-  # may raise; returns true or false
-  def self.verify(xopk, msg, sig)
-    raise(TypeError, "xopk: #{XOPK.name}") unless xopk.is_a? XOPK
-    raise(TypeError, "msg: string") unless msg.is_a? String
-    raisE(TypeError, "sig: #{SSS.name}") unless sig.is_a? SSS
-    sig.verify(msg, xopk)
+  # false for a verification failure, true otherwise; may raise
+  def self.verify(pk, m, sig)
+    raise(TypeError, "pk: string") unless pk.is_a? String
+    raise(SizeError, "pk: 32 bytes") unless pk.bytesize == 32
+    raise(EncodingError, "pk: binary") unless pk.encoding == Encoding::BINARY
+    raise(TypeError, "m: string") unless m.is_a? String
+    raise(TypeError, "sig: string") unless sig.is_a? String
+    raise(SizeError, "sig: 64 bytes") unless sig.bytesize == 64
+    raise(EncodingError, "sig: binary") unless sig.encoding == Encoding::BINARY
+
+    begin
+      xopk = Secp256k1::XOnlyPublicKey.from_data(pk)
+    rescue Secp256k1::Error
+      return false
+    end
+
+    signature(sig).verify(m, xopk)
   end
 
-  def self.keypair(sk = nil)
+  # returns Secp256k1::KeyPair
+  def self.key_pair(sk = nil)
     if sk
       raise(TypeError, "sk: string") unless sk.is_a? String
       CONTEXT.key_pair_from_private_key(sk)
@@ -29,6 +41,13 @@ module SchnorrFast
     end
   end
 
+  # returns [sk, pk]
+  def self.keypair(sk = nil)
+    kp = self.key_pair(sk)
+    [kp.private_key.data, kp.xonly_public_key.serialized]
+  end
+
+  # returns Secp256k1::SchnorrSignature
   def self.signature(str)
     raise(TypeError, "str: string") unless str.is_a? String
     Secp256k1::SchnorrSignature.from_data(str)
--- test/schnorr_fast/vectors.rb
+++ test/schnorr_fast/vectors.rb
@@ -1,13 +1,11 @@
 require 'csv'
-require 'rbsecp256k1'
+require 'schnorr_fast'
 
 path = File.join(__dir__, '..', 'vectors.csv')
 table = CSV.read(path, headers: true)
 
 success = []
 failure = []
-
-CONTEXT = Secp256k1::Context.create
 
 def hex2bin(hex)
   [hex].pack('H*')
@@ -33,42 +31,10 @@ table.each { |row|
   comment  = row.fetch('comment')
   result   = row.fetch('verification result') == 'TRUE'
 
-  if sk.empty?
-    begin
-      xopk = Secp256k1::XOnlyPublicKey.from_data(pk)
-    rescue Secp256k1::DeserializationError
-      if !result
-        success << row
-      else
-        warn "index #{index} failed verification; expected success"
-        failure << row
-      end
-      next
-    end
+  if SchnorrFast.verify(pk, m, sig) == result
+    success << row
   else
-    keypair = CONTEXT.key_pair_from_private_key(sk)
-    xopk = keypair.xonly_public_key
-
-    if xopk.serialized != pk
-      warn "index #{index} generated public key does not match vectors.csv"
-    end
-  end
-  sig = Secp256k1::SchnorrSignature.from_data(sig)
-
-  begin
-    if sig.verify(m, xopk) == result
-      success << row
-    else
-      warn "index #{index} passed verification; expected failure"
-      failure << row
-    end
-  rescue Secp256k1::Error => e
-    if !result
-      success << row
-    else
-      warn "index #{index} failed verification; expected success"
-      failure << row
-    end
+    failure << row
   end
   print '.'
 }
@@ -76,7 +42,5 @@ puts
 
 puts "Success: #{success.count}"
 puts "Failure: #{failure.count}"
-
-failure.each { |row| p row }
 
 exit failure.count
