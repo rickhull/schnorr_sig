e43631d1f2886bfcee37432b34e58c8be6b02b1a
Rick Hull <rick.hull@gmail.com>
2024-06-03 18:15:29 -0400

    rescue LoadError, require pure

--- lib/schnorr_sig.rb
+++ lib/schnorr_sig.rb
@@ -1,5 +1,10 @@
 if ENV['SCHNORR_SIG']&.downcase == 'fast'
-  require 'schnorr_sig/fast'
+  begin
+    require 'schnorr_sig/fast'
+  rescue LoadError
+    warn "LoadError: schnorr_sig/fast cannot be loaded"
+    require 'schnorr_sig/pure'
+  end
 else
   require 'schnorr_sig/pure'
 end
