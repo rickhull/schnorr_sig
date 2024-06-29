51340dc7eabbc8a73a19f5cea6ad75dd7d88b44f
Rick Hull <rick.hull@gmail.com>
2024-06-29 10:32:07 -0400

    flesh out test/pure.rb

--- test/pure.rb
+++ test/pure.rb
@@ -43,6 +43,48 @@ describe SchnorrSig do
   end
 
   it "converts an integer or point to a binary string" do
+    str = SchnorrSig.bytes(0)
+    expect(str).must_be_kind_of String
+    expect(str.length).must_equal 32
+    expect(str).must_equal ("\x00" * 32).b
 
+    p = SchnorrSig.point(1234)
+    expect(SchnorrSig.bytes(p)).must_equal SchnorrSig.big2bin(p.x)
+  end
+
+  it "signs a message" do
+    sk = Random.bytes(32)
+    m = 'hello world'
+    sig = SchnorrSig.sign(sk, m)
+    expect(sig).must_be_kind_of String
+    expect(sig.length).must_equal 64
+    expect(sig.encoding).must_equal Encoding::BINARY
+  end
+
+  it "implements tagged hashes" do
+    # SHA256.digest
+    h = SchnorrSig.tagged_hash('BIP0340/challenge', 'hello world')
+    expect(h).must_be_kind_of String
+    expect(h.length).must_equal 32
+    expect(h.encoding).must_equal Encoding::BINARY
+  end
+
+  it "verifies signatures" do
+    sk, pk = SchnorrSig.keypair
+    m = 'hello world'
+    sig = SchnorrSig.sign(sk, m)
+    expect(SchnorrSig.verify?(pk, m, sig)).must_equal true
+  end
+
+  it "implements lift_x()" do
+    expect(SchnorrSig.lift_x(1)).must_be_kind_of ECDSA::Point
+  end
+
+  it "generates a pubkey for any secret key value" do
+    sk = Random.bytes(32)
+    pk = SchnorrSig.pubkey(sk)
+    expect(pk).must_be_kind_of String
+    expect(pk.length).must_equal 32
+    expect(pk.encoding).must_equal Encoding::BINARY
   end
 end
