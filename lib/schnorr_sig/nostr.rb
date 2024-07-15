f8fb759d27d594bde1509200bb95b929dfde8236
Rick Hull <rick.hull@gmail.com>
2024-07-15 16:30:58 -0400

    reorganize and add tests

--- lib/schnorr_sig/nostr.rb
+++ lib/schnorr_sig/nostr.rb
@@ -2,6 +2,9 @@ require 'schnorr_sig'
 require 'json'
 require 'digest'
 
+# this implements most/all of NIP-01, which is the only _required_ spec
+# see https://github.com/nostr-protocol/nips/blob/master/01.md
+
 module SchnorrSig
   module Nostr
     class Error < RuntimeError; end
@@ -9,15 +12,14 @@ module SchnorrSig
     class EncodingError < Error; end
     class SizeError < Error; end
 
-    # raise or return val
-    def self.typecheck!(val, cls)
-      raise(TypeError, "#{cls} : #{val.inspect}") unless val.is_a? cls
-      val
+    # raise SchnorrSig::TypeError or return str
+    def self.string!(str)
+      SchnorrSig.string!(str) and str
     end
 
-    # raise or return str
+    # raise (EncodingError, SizeError) or return str
     def self.binary!(str, length = nil)
-      Nostr.typecheck!(str, String)
+      string!(str)
       raise(EncodingError, str.encoding) if str.encoding != Encoding::BINARY
       if length and length != str.bytesize
         raise(SizeError, "#{length} : #{str.bytesize}")
@@ -27,14 +29,15 @@ module SchnorrSig
 
     # raise or return str
     def self.hex!(str, length = nil)
-      Nostr.typecheck!(str, String)
+      string!(str)
       raise(EncodingError, str.encoding) if str.encoding == Encoding::BINARY
-      if length and length != str.bytesize
-        raise(SizeError, "#{length} : #{str.bytesize}")
+      if length and length != str.length
+        raise(SizeError, "#{length} : #{str.length}")
       end
       str
     end
 
+    # per NIP-01
     JSON_OPTIONS = {
       allow_nan: false,
       max_nesting: 3,
@@ -55,80 +58,6 @@ module SchnorrSig
     # convert a ruby object, likely hash or array, return a string of JSON
     def self.json(object)
       JSON.generate(object, **JSON_OPTIONS)
-    end
-
-    # this class stores user profile info, keys, and is responsible for
-    # creating events (messages, etc)
-    class User
-      attr_reader :name, :about, :picture, :sk, :pk
-
-      def initialize(name:, about: '', picture: '', sk: nil, pk: nil)
-        @name = Nostr.typecheck!(name, String)
-        @about = Nostr.typecheck!(about, String)
-        @picture = Nostr.typecheck!(picture, String)
-        if sk
-          @sk = Nostr.binary!(sk, 32)
-          @pk = pk.nil? ? SchnorrSig.pubkey(@sk) : Nostr.binary!(pk, 32)
-        else
-          @sk, @pk = SchnorrSig.keypair
-        end
-      end
-
-      def pubkey
-        SchnorrSig.bin2hex @pk
-      end
-
-      # returns an Event
-      def new_event(content, kind:)
-        Event.new(content, kind: kind, pubkey: self.pubkey)
-      end
-
-      # returns 64 bytes binary
-      def sign(event)
-        event.sign(@sk)
-      end
-
-      # returns an Event, kind: 1, text_note
-      def text_note(content)
-        new_event(content, kind: :text_note)
-      end
-
-      # Input
-      #   (about: string)
-      #   (picture: string)
-      # Output
-      #   Event
-      #     kind: 0, set_metadata
-      #     content: {
-      #       name: <username>, about: <string>, picture: <url, string>
-      #     }
-      def set_metadata(about: nil, picture: nil, **kwargs)
-        @about = about if about and about != @about
-        @picture = picture if picture and picture != @picture
-        hash = kwargs.merge({ name:    @name,
-                              about:   Nostr.typecheck!(@about, String),
-                              picture: Nostr.typecheck!(@picture, String), })
-
-        new_event(Nostr.json(hash), kind: :set_metadata)
-      end
-      alias_method :profile, :set_metadata
-
-      # Input
-      #   pubkey_hsh: a ruby hash of the form
-      #     "deadbeef1234abcdef" => ["wss://alicerelay.com/", "alice"]
-      def contact_list(pubkey_hsh)
-        list = new_event('', kind: :contact_list)
-        pubkey_hsh.each { |pubkey, ary|
-          list.ref_pubkey(Nostr.hex!(pubkey, 64), *(ary or Array.new))
-        }
-        list
-      end
-      alias_method :follows, :contact_list
-
-      def encrypted_text_message(content)
-        new_event(content, kind: :encrypted_text_message)
-      end
-      alias_method :direct_msg, :encrypted_text_message
     end
 
     class Event
@@ -190,7 +119,7 @@ module SchnorrSig
       attr_reader :content, :kind, :created_at, :pubkey, :signature
 
       def initialize(content = '', kind: :text_note, pubkey:)
-        @content = Nostr.typecheck!(content, String)
+        @content = Nostr.string!(content)
         @kind = Event.kind(kind)
         @pubkey = Nostr.hex!(pubkey, 64)
         @tags = []
@@ -230,7 +159,7 @@ module SchnorrSig
       end
 
       def signed?
-        @signature and @signature.bytesize == 64 # steep:ignore
+        !!@signature and @signature.bytesize == 64 # steep:ignore
       end
 
       def signed!
@@ -264,9 +193,8 @@ module SchnorrSig
       # add an array of 2+ strings to @tags
       def add_tag(tag, value, *rest)
         @digest = nil # invalidate any prior digest
-        @tags.push([Nostr.typecheck!(tag, String),
-                    Nostr.typecheck!(value, String)] +
-                   rest.each { |s| Nostr.typecheck!(s, String) })
+        @tags.push([Nostr.string!(tag), Nostr.string!(value)] +
+                   rest.each { |s| Nostr.string!(s) })
       end
 
       # add an event tag based on event id, hex encoded
@@ -286,6 +214,80 @@ module SchnorrSig
         val = [Event.kind(kind), Nostr.hex!(pubkey, 64), d_tag].join(':')
         add_tag('a', val, *rest)
       end
+    end
+
+    # this class stores user profile info, keys, and is responsible for
+    # creating events (messages, etc)
+    class User
+      attr_reader :name, :about, :picture, :sk, :pk
+
+      def initialize(name:, about: '', picture: '', sk: nil, pk: nil)
+        @name = Nostr.string!(name)
+        @about = Nostr.string!(about)
+        @picture = Nostr.string!(picture)
+        if sk
+          @sk = Nostr.binary!(sk, 32)
+          @pk = pk.nil? ? SchnorrSig.pubkey(@sk) : Nostr.binary!(pk, 32)
+        else
+          @sk, @pk = SchnorrSig.keypair
+        end
+      end
+
+      def pubkey
+        SchnorrSig.bin2hex @pk
+      end
+
+      # returns an Event
+      def new_event(content, kind:)
+        Event.new(content, kind: kind, pubkey: self.pubkey)
+      end
+
+      # returns 64 bytes binary
+      def sign(event)
+        event.sign(@sk)
+      end
+
+      # returns an Event, kind: 1, text_note
+      def text_note(content)
+        new_event(content, kind: :text_note)
+      end
+
+      # Input
+      #   (about: string)
+      #   (picture: string)
+      # Output
+      #   Event
+      #     kind: 0, set_metadata
+      #     content: {
+      #       name: <username>, about: <string>, picture: <url, string>
+      #     }
+      def set_metadata(about: nil, picture: nil, **kwargs)
+        @about = about if about and about != @about
+        @picture = picture if picture and picture != @picture
+        hash = kwargs.merge({ name:    @name,
+                              about:   Nostr.string!(@about),
+                              picture: Nostr.string!(@picture), })
+
+        new_event(Nostr.json(hash), kind: :set_metadata)
+      end
+      alias_method :profile, :set_metadata
+
+      # Input
+      #   pubkey_hsh: a ruby hash of the form
+      #     "deadbeef1234abcdef" => ["wss://alicerelay.com/", "alice"]
+      def contact_list(pubkey_hsh)
+        list = new_event('', kind: :contact_list)
+        pubkey_hsh.each { |pubkey, ary|
+          list.ref_pubkey(Nostr.hex!(pubkey, 64), *(ary or Array.new))
+        }
+        list
+      end
+      alias_method :follows, :contact_list
+
+      def encrypted_text_message(content)
+        new_event(content, kind: :encrypted_text_message)
+      end
+      alias_method :direct_msg, :encrypted_text_message
     end
   end
 end
---
+++ test/nostr.rb
@@ -0,0 +1,183 @@
+require 'schnorr_sig/nostr'
+require 'minitest/autorun'
+
+include SchnorrSig
+
+describe Nostr do
+  describe "module functions" do
+    it "asserts an expected string is actually a string" do
+      expect { Nostr.string!(1234) }.must_raise SchnorrSig::TypeError
+      expect(Nostr.string!('asdf')).must_equal 'asdf'
+    end
+
+    it "asserts an expected binary string is actually binary" do
+      expect { Nostr.binary!('asdf') }.must_raise Nostr::EncodingError
+      expect(Nostr.binary!('asdf'.b)).must_equal 'asdf'.b
+    end
+
+    it "optionally asserts the length of an expected binary string" do
+      expect { Nostr.binary!("\x00".b, 2) }.must_raise Nostr::SizeError
+      expect(Nostr.binary!("\x00".b, 1)).must_equal "\x00".b
+    end
+
+    it "asserts an expected hex string is not actually binary" do
+      expect { Nostr.hex!('abcd'.b) }.must_raise Nostr::EncodingError
+      expect(Nostr.hex!('abcd')).must_equal 'abcd'
+    end
+
+    it "optionally asserts the length of an expected hex string" do
+      expect { Nostr.hex!("00", 1) }.must_raise Nostr::SizeError
+      expect(Nostr.hex!("00", 2)).must_equal "00"
+    end
+
+    it "parses a JSON string to a Ruby object" do
+      expect(Nostr.parse('{}')).must_equal Hash.new
+      expect(Nostr.parse('[]')).must_equal Array.new
+    end
+
+    it "generates JSON from a Ruby hash or array" do
+      expect(Nostr.json({})).must_equal '{}'
+      expect(Nostr.json([])).must_equal '[]'
+    end
+  end
+
+  describe Nostr::Event do
+    E = Nostr::Event
+    SK, PK = SchnorrSig.keypair
+
+    describe "class functions" do
+      it "determines the _kind_ integer from an integer or symbol" do
+        # any integer except 2 is valid
+        # only a few symbols are valid
+        expect(E.kind(0)).must_equal 0
+        expect(E.kind(:set_metadata)).must_equal 0
+        expect(E.kind(1)).must_equal 1
+        expect(E.kind(:text_note)).must_equal 1
+        expect { E.kind(2) }.must_raise Nostr::DeprecatedError
+        expect { E.kind(:recommend_server) }.must_raise Nostr::DeprecatedError
+        expect(E.kind(:contact_list)).must_equal 3
+        expect(E.kind(:encrypted_direct_message)).must_equal 4
+        expect(E.kind(5)).must_equal 5
+        expect { E.kind(:unknown) }.must_raise
+      end
+
+      it "signs a message given a secret key" do
+        sig = E.sign('asdf', SK)
+        expect(sig).must_be_kind_of String
+        expect(sig.length).must_equal 64
+        expect(sig.encoding).must_equal Encoding::BINARY
+      end
+    end
+
+    def new_event(msg = 'test')
+      E.new(msg, pubkey: SchnorrSig.bin2hex(PK))
+    end
+
+    it "requires a message and a hex(64) pubkey to initialize" do
+      expect { E.new('asdf') }.must_raise
+      event = E.new('test event', pubkey: SchnorrSig.bin2hex(PK))
+      expect(event).must_be_kind_of E
+      expect(event.kind).must_equal 1
+    end
+
+    it "serializes to a Ruby array of length 6" do
+      e = new_event()
+      a = e.serialize
+      expect(a).must_be_kind_of Array
+      expect(a).wont_be_empty
+      expect(a.length) == 6
+    end
+
+    it "converts the array[6] to JSON, then SHA256 for 32B digest" do
+      e = new_event()
+      d = e.digest
+      expect(d).must_be_kind_of String
+      expect(d.encoding).must_equal Encoding::BINARY
+      expect(d.length).must_equal 32
+    end
+
+    it "converts the digest to 64B hex encoding for the _id_ field" do
+      e = new_event()
+      i = e.id
+      expect(i).must_be_kind_of String
+      expect(i.encoding).wont_equal Encoding::BINARY
+      expect(i.length).must_equal 64
+    end
+
+    it "signs the event with any 32B secret key yielding 64B sig" do
+      e = new_event()
+      s = e.sign(SK)
+      expect(s).must_be_kind_of String
+      expect(s.encoding).must_equal Encoding::BINARY
+      expect(s.length).must_equal 64
+    end
+
+    it "checks whether the event has been signed" do
+      e = new_event()
+      expect(e.signed?).must_equal false
+      expect { e.signed! }.must_raise E::SignatureMissing
+
+      e.sign(SK)
+      expect(e.signed?).must_equal true
+      expect(e.signed!).must_equal true
+    end
+
+    it "provides _sig_ as 128B of hex" do
+      e = new_event()
+      e.sign(SK)
+      sig = e.sig
+      expect(sig).must_be_kind_of String
+      expect(sig.encoding).wont_equal Encoding::BINARY
+      expect(sig.length).must_equal 128
+    end
+
+    it "provides _object_hash_ as a Ruby hash, suitable for JSON conversion" do
+      e = new_event()
+      expect { e.object_hash }.must_raise E::SignatureMissing
+
+      e.sign(SK)
+      h = e.object_hash
+      expect(h).must_be_kind_of Hash
+      expect(h).wont_be_empty
+      expect(h.length).must_equal 7
+    end
+
+    it "provides _json_object_ as a NIPS01 Event object, a string of JSON" do
+      e = new_event()
+      expect { e.json_object }.must_raise E::SignatureMissing
+
+      e.sign(SK)
+      j = e.json_object
+
+      # id + pubkey + sig == 256 bytes
+
+      expect(j).must_be_kind_of String
+      expect(j).wont_be_empty
+      expect(j.encoding).wont_equal Encoding::BINARY
+      expect(j.length).must_be :>, 256
+    end
+  end
+
+  describe Nostr::User do
+    it "has a name, about, picture, and keypair (sk and pk)" do
+      u = Nostr::User.new(name: 'test')
+      expect(u).must_be_kind_of Nostr::User
+      expect(u.name).must_equal 'test'
+      expect(u.about).must_equal ''
+      expect(u.picture).must_equal ''
+
+      sk, pk = u.sk, u.pk
+      expect(sk).must_be_kind_of String
+      expect(sk.length).must_equal 32
+      expect(pk).must_be_kind_of String
+      expect(pk.length).must_equal 32
+      expect(sk).wont_equal pk
+    end
+
+    it "provides a hex representation of the public key" do
+      u = Nostr::User.new(name: 'test')
+      hex = u.pubkey
+      expect(Nostr.hex!(hex, 64)).must_equal hex
+    end
+  end
+end
