959afb8a1f3f634f28eae253ae051f5c2df3b46a
Rick Hull <rick.hull@gmail.com>
2024-07-25 19:43:10 -0400

    trivial; add comments

--- examples/nostr_relay.rb
+++ examples/nostr_relay.rb
@@ -23,6 +23,7 @@ puts
 
 # msg is ready for delivery
 # relay receives JSON string
+
 puts "Relay receives:"
 puts msg.to_json
 puts
@@ -30,16 +31,22 @@ puts
 # verify the signature
 # if no errors raised, signature is verified
 # decompose into fields (Ruby hash buckets)
+
 hash = Event.verify(msg.to_json)
 puts "Signature verified:"
 p hash
 puts
 
+# show that the relay faithfully recreates the original serialization
+# this is required to verify the id and ultimately the signature
+# which we have already done
+
 puts "Relay serialization:"
 puts Event.serialize(hash)
 puts
 
-# send received json to the the destination
+# if we had to: send received json to the the destination
+
 puts "Relay sends:"
 puts msg.to_json
 puts
