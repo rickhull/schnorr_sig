266c28957f5e816c4e5d857629d77fe91225fba2
Rick Hull <rick.hull@gmail.com>
2024-09-02 11:10:08 -0400

    add output

--- examples/both.rb
+++ examples/both.rb
@@ -9,8 +9,16 @@ msg = 'hello world'
 hsh = Fast.tagged_hash('message', msg)
 raise("hash mismatch") if Pure.tagged_hash('message', msg) != hsh
 
+puts "Message: #{msg}"
+puts "Tagged Hash: #{Fast.bin2hex(hsh)}"
+puts
+
 sigp = Pure.sign(sk, hsh)
 sigf = Fast.sign(sk, hsh)
+
+puts "Pure Sig: #{Pure.bin2hex(sigp)}"
+puts "Fast Sig: #{Fast.bin2hex(sigf)}"
+puts
 
 puts "Fast.verify?(Pure.sign): #{Fast.verify?(pk, hsh, sigp)}"
 puts "Pure.verify?(Fast.sign): #{Pure.verify?(pk, hsh, sigf)}"
