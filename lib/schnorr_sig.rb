a97c2215af244221e144c1bd3f995f0fc284d6fc
Rick Hull <rick.hull@gmail.com>
2024-09-16 13:41:07 -0400

    remove ENV['SCHNORR_SIG']; always try schnorr_sig/fast

--- lib/schnorr_sig.rb
+++ lib/schnorr_sig.rb
@@ -1,16 +1,7 @@
-loaded = false
-
-if ENV['SCHNORR_SIG']&.downcase == 'fast'
-  begin
-    require 'schnorr_sig/fast'
-    SchnorrSig.extend SchnorrSig::Fast
-    loaded = true
-  rescue LoadError => e
-    warn [e.class, e.message].join(': ')
-  end
-end
-
-unless loaded
+begin
+  require 'schnorr_sig/fast'
+  SchnorrSig.extend SchnorrSig::Fast
+rescue LoadError
   require 'schnorr_sig/pure'
   SchnorrSig.extend SchnorrSig::Pure
 end
