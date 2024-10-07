4d971d6404fc6faed9b76f5df8bfbd3ad62ff2a8
Rick Hull <rick.hull@gmail.com>
2024-10-07 11:07:32 -0400

    add nixpkgs for installing rbsecp256k1

--- default.nix
+++ default.nix
@@ -1,4 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ ruby_3_3 ];
-}
+    nativeBuildInputs = with pkgs.buildPackages; [
+      ruby_3_3
+      bundix
+      autoconf
+      automake
+      libtool
+      secp256k1
+    ];
+  }
