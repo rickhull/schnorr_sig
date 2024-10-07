b8fe1b09a6d773db13d6f406701756970f5c1127
Rick Hull <rick.hull@gmail.com>
2024-10-07 11:59:30 -0400

    cleanup, don't use bundix

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [
-      ruby_3_3
-      bundix
-      autoconf
-      automake
-      libtool
-      secp256k1
-    ];
-  }
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    ruby_3_3
+    autoconf
+    automake
+    libtool
+    secp256k1
+  ];
+}
